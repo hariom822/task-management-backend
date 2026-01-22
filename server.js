04888e2afcc0807ad14474686269c940d9029c16
Hariom Sharma <hariom822@gmail.com>
2026-01-22 14:31:58 +0530

    Fix MongoDB URI and JWT config

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const fileupload=require("express-fileupload")
 const app = express();
 const port=process.env.PORT;
 
-const url =process.env.URL
+const url =process.env.MONGO_URI;
+console.log(url)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
